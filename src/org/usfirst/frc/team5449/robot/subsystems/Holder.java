56670e8f3547811d34880cd4364ae7f1df447fab
infinity1096 <2315465052@qq.com>
2018-01-07 17:11:30 +0800

    Adds Holder

---
+++ src/org/usfirst/frc/team5449/robot/subsystems/Holder.java
@@ -0,0 +1,27 @@
+package org.usfirst.frc.team5449.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class Holder extends Subsystem{
+	
+	
+	Holder(){
+		
+	}
+	
+	public void open(){
+		
+	} 
+	
+	public void close(){
+		
+	}
+	
+	@Override
+	protected void initDefaultCommand() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+}
